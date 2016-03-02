f1b5b23ad4f87c30caf41a42befd8c17e4138871
Brian Coia <bcoia@me.com>
2016-03-02 00:29:53 -0600

    Switched from Oh-My-Zsh to Prezto

--- zsh/install.sh
+++
@@ -1,6 +0,0 @@
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Install plugins
-git clone git://github.com/tarruda/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone git://github.com/chrissicool/zsh-256color $ZSH_CUSTOM/plugins/zsh-256color
